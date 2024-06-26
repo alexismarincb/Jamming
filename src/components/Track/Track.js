cac3a2b097171d213253b7b4fff68508301e6e3f
Alexis Marin <alexismarincb@gmail.com>
2024-06-26 11:57:06 -0600

    I added a hardcode (simulation) of tracks to check

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './Track.css';
 
+
+const tracks = [
+    { id: 1, name: 'Track 1', artist: 'Artist 1', album: 'Album 1' },
+    { id: 2, name: 'Track 2', artist: 'Artist 2', album: 'Album 2' },
+    { id: 3, name: 'Track 3', artist: 'Artist 3', album: 'Album 3' }
+    // Add more tracks as needed
+  ];
+
+
 function Track() {
   return (
     <div className="Track">
       <div className="Track-information">
-        <h3>Track Name</h3>
+        <h3>Track Name: </h3>
         <p>Artist | Album</p>
       </div>
       <button className="Track-action">+</button>
