3d42b65d1e405a2278854f22a88dbccd1bdaed9f
Alexis Marin <alexismarincb@gmail.com>
2024-06-27 17:24:52 -0600

    Allows the + and - to change

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Track.css';
 
-function Track({ track }) { // Destructure props directly here
+function Track({ track }) {
+    const [plusOrMinus, setPlusOrMinus] = useState("yes");
+
     return (
         <div className="Track">
             <div className="Track-information">
-                <h3>Track Name: {track.name}</h3> 
-                <p>Artist: {track.artist} | Album: {track.album}</p> 
+                <h3>Track Name: {track.name}</h3>
+                <p>Artist: {track.artist} | Album: {track.album}</p>
             </div>
-            <button className="Track-action">+</button>
+            {plusOrMinus === "yes" ? (
+                <button className="Track-action">+</button>
+            ) : (
+                <button className="Track-action">-</button>
+            )}
         </div>
     );
 }
