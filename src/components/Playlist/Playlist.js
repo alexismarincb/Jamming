04796b62e7efa564feade04ae1b9521bca29b570
alexanderadavid <alexander.david@ultimaker.com>
2024-06-28 13:51:25 -0700

    Fix the missing function error

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,31 +1,30 @@
-import React, { useState } from 'react';
-import './Playlist.css';
-import Track from '../Track/Track';
+import React, { useState } from "react";
+import "./Playlist.css";
+import Track from "../Track/Track";
 
-function Playlist() {
-    const [playlist, setPlaylist] = useState([]);
+function Playlist({ playlist, addTrackToPlaylist }) {
+  // This function adds a track to the playlist
 
-    // This function adds a track to the playlist
-    const addTrackToPlaylist = (track) => {
-        setPlaylist(prevPlaylist => [...prevPlaylist, track]); // Using functional update for state
-    };
-
-    return (
-        <div className="Playlist">
-            <h2>Playlist</h2>
-            <input value="New Playlist" />
-            {playlist.length > 0 ? (
-                <>
-                    {playlist.map(track => (
-                        <Track key={track.id} track={track} addTrackToPlaylist={addTrackToPlaylist} />
-                    ))}
-                    <button className="Playlist-save">SAVE TO SPOTIFY</button>
-                </>
-            ) : (
-                <h3>EMPTY PLAYLIST</h3>
-            )}
-        </div>
-    );
+  return (
+    <div className="Playlist">
+      <h2>Playlist</h2>
+      <input value="New Playlist" />
+      {playlist.length > 0 ? (
+        <>
+          {playlist.map((track) => (
+            <Track
+              key={track.id}
+              track={track}
+              addTrackToPlaylist={addTrackToPlaylist}
+            />
+          ))}
+          <button className="Playlist-save">SAVE TO SPOTIFY</button>
+        </>
+      ) : (
+        <h3>EMPTY PLAYLIST</h3>
+      )}
+    </div>
+  );
 }
 
 export default Playlist;
--- src/components/Tracklist/Tracklist.js
+++ src/components/Tracklist/Tracklist.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import './Tracklist.css';
-import Track from '../Track/Track';
+import React from "react";
+import "./Tracklist.css";
+import Track from "../Track/Track";
 
-function Tracklist({ tracks, trackOrPlay }) {
-
-    return (
-        <div className="Tracklist">
-            {tracks.map(track => (
-                <Track key={track.id} track={track} trackOrPlay= {trackOrPlay} />
-            ))}
-        </div>
-    );
+function Tracklist({ tracks, trackOrPlay, addTrackToPlaylist }) {
+  return (
+    <div className="Tracklist">
+      {tracks.map((track) => (
+        <Track
+          key={track.id}
+          track={track}
+          trackOrPlay={trackOrPlay}
+          addTrackToPlaylist={addTrackToPlaylist}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Tracklist;
